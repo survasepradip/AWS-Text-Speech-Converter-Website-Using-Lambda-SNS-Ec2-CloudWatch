36a247574748317f0db7afdb0c4c51c0932b6e38
survasepradip <survaseps145@g,mail.com>
2023-02-03 10:09:17 +0530

    commit

---
+++ pollyassets/dpjs.js
@@ -0,0 +1,24 @@
+// string ,numbers,bollean, null, undefined 
+
+const name ='joh ;
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined ;
+let z;
+
+console.log(typeof,isCool)
+
+//concat
+
+const name ='joh ;
+const age = 30;
+console.log('my name is john'+ name +' and i am '+ age);
+
+//template
+const hello ='my name is ${name} and i am ${age}';
+console.log('my name is ${name}
+
+
+
---
+++ pollyassets/np.py
@@ -0,0 +1,6 @@
+height =20
+reduis =10
+ volume = (3.14 * radius * radius * height)+ (2*3.14 * radius * radius)
+ 
+ 
+ print('a cylinder , volume,'heigh)
